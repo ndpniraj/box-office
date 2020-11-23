9cb3eaa050e43b5ed96ddec775df41a91774da4e
NDP Niraj <ndpniraj@gmail.com>
2020-11-23 22:17:30 +0530

    added radio button and filter inside api url

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,16 @@ import { getApi } from '../misc/config';
 const Home = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsSelected = searchOption === 'shows';
 
   const onInputChange = ev => {
     setInput(ev.target.value);
   };
 
   const handleSearch = () => {
-    getApi(`/search/shows?q=${input}`).then(result => {
+    getApi(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
     });
   };
@@ -28,17 +31,45 @@ const Home = () => {
     }
 
     if (results && results.length > 0) {
-      return results.map(item => (
-        <div key={item.show.id}>{item.show.name}</div>
-      ));
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
     }
 
     return null;
   };
 
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
   return (
     <MainPageLayout>
       <input onChange={onInputChange} value={input} onKeyDown={handleKeyDown} />
+      <div>
+        <label htmlFor="select-shows">
+          <input
+            type="radio"
+            id="select-shows"
+            value="shows"
+            checked={isShowsSelected}
+            onChange={onRadioChange}
+          />
+          Shows
+        </label>
+        <label htmlFor="select-person">
+          <input
+            type="radio"
+            id="select-person"
+            value="people"
+            checked={!isShowsSelected}
+            onChange={onRadioChange}
+          />
+          Actors
+        </label>
+      </div>
       <button type="button" onClick={handleSearch}>
         Search
       </button>
