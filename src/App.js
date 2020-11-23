56fe70e79f3f91cac69559742d2fc50b89ce06b3
NDP Niraj <ndpniraj@gmail.com>
2020-11-23 20:40:58 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is the home page.
+      </Route>
+      <Route exact path="/starred">
+        This is the starred page.
+      </Route>
+      <Route>This is the 404 page</Route>
+    </Switch>
+  );
 }
 
 export default App;
