678f7548b822941214934f1b841df752b72c50b0
NDP Niraj <ndpniraj@gmail.com>
2020-11-23 21:29:07 +0530

    added search bar and serch logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>This is the home page</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const handleSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(response => response.json())
+      .then(result => console.log(result));
+  };
+
+  const handleKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      handleSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input onChange={onInputChange} value={input} onKeyDown={handleKeyDown} />
+      <button type="button" onClick={handleSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
