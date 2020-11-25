79988da2ffb2198c73a1e804053a17e7a28fe683
NDP Niraj <ndpniraj@gmail.com>
2020-11-25 20:31:15 +0530

    defined theme for styled component and created few styled components.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
 
 import './index.css';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/starred" component={Starred} />
-      <Route exact path="/show/:id" component={Show} />
-      <Route>This is the 404 page</Route>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/starred" component={Starred} />
+        <Route exact path="/show/:id" component={Show} />
+        <Route>This is the 404 page</Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
+import { NavList, LinkStyled } from './Navs.styled';
 
 const Links = [
   { to: '/', text: 'Home' },
@@ -7,14 +8,21 @@ const Links = [
 ];
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
-    <ul>
+    <NavList>
       {Links.map(item => (
         <li key={item.to}>
-          <Link to={item.to}>{item.text}</Link>
+          <LinkStyled
+            to={item.to}
+            className={location.pathname === item.to ? 'active' : ''}
+          >
+            {item.text}
+          </LinkStyled>
         </li>
       ))}
-    </ul>
+    </NavList>
   );
 };
 
